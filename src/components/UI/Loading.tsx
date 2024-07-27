95876108a64e293360fb0cb59cde73ffffb72520
Robert Santos <robertdahora@gmail.com>
2024-07-27 15:38:15 -0300

    feat: add Loading UI component

---
+++ src/components/UI/Loading.tsx
@@ -0,0 +1,18 @@
+import type { FC } from "react"
+
+import styles from "./loading.module.css"
+
+interface Props {
+  width?: string
+  height?: string
+}
+
+const Loading: FC<Props> = ({ width = "100%", height = "100%" }) => {
+  return (
+    <div style={{ width, height }} className={styles.container}>
+      <span className={styles.loader}></span>
+    </div>
+  )
+}
+
+export default Loading
